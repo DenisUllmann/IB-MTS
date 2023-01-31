3cd559249918d7c189c6acd050ff7de34f86e47f
DenisUllmann <31477905+DenisUllmann@users.noreply.github.com>
2023-01-31 17:44:41 +0100

    Update main.py
    use absl.app and absl.flags

--- main.py
+++ main.py
@@ -8,6 +8,9 @@ Created on Tue Aug 13 23:24:05 2019
 import os
 import tensorflow as tf
 import numpy as np
+from absl import app
+from absl import flags
+from absl import logging
 
 from models import SP_PCUNet
 
@@ -87,59 +90,55 @@ classes_and_inclusions_addnoclass = list(zip(
 #      'noclass',
 #      'noclass']))
 
-class Flags(object):
-    def __init__(self):
-        self.model_type = "PCUNet"# name of the model to user ['PCUNet'], ['LSTM'], ['GRU'], ..")
-        self.with_centerloss = False# whether to add a term in the total loss optimizing the proximity to the centers
-        self.debug = True# True to use debug mode (1 epoch and 1st item of generator for test)")
-        self.epoch = 100# Epoch to train [25]")
-        self.batch_size = 4# The size of batch images [4]")
-        self.batch_norm = True# True for the model with batch_normalzation")
-        self.learning_rate_BN = 0.0002# Learning rate of for adam with BN (phase 1) [0.0002]")
-        self.learning_rate_FINE = 0.00005# Learning rate of for adam without BN (phase 2 - Fine tuning) [0.00005]")
-        self.dataset = "pb_2C"# The name of dataset [iris_level_2C, UCSD, mnist]")
-        self.dataset_address = 'C:/Users/Denis/ML/MTS_Data/PEMS-BAY/data_2c'# The path of dataset")
-        self.given_tvt = True# Whether the data is already separated in 'train' 'valid' 'test' (these should appear in the file names)
-        self.train_ratio = 0.7# ratio of dataset to use for training [0.7]")
-        self.test_ratio = 0.25# ratio of dataset to use for testing [0.25]")
-        self.label_length = 325# The length of spectra. [240 (for Mghk), 137 (for al), 370 (for ld)))]")
-        if self.model_type == "NBeats":
-            self.n_blocks = int(65714097/50400)
-        self.mask_ratio = 0.25# ending ratio of the timesequences to be masked in time / max ratio is random_ratio in True")
-        self.random_ratio = False# True for random ending ratio of the timesequences to be masked in time (with max value = mask_ratio)")
-        self.labels = '_'.join(['PB'])# label for training ['QS','AR','PF','FL'] (these should appear in the filename[:2])")
-        self.nolabel = None# allow to sample from unlabeled data and label it eg. 'nolabel'")
-        self.test_labels = '_'.join(['PB'])# label for testing ['QS','AR','PF','FL']")
-        self.name = 'model%s%s_B%i_M%i_R%i_%s'%([self.model_type+"%s"%['','star'][int(self.with_centerloss)],"Mghk%s"%['','star'][int(self.with_centerloss)]][int(self.model_type=="PCUNet")], self.dataset[-2:], self.batch_size, int(100*self.mask_ratio), int(self.random_ratio), self.labels)# The name of the model")
-        self.checkpoint_dir = "C:/Users/Denis/ML/IRIS_predspectra_intermediate_tf2/%s/%s/checkpoint"%(self.dataset,self.name)# Directory name to save the checkpoints [checkpoint]")
-        self.logs_dir = "C:/Users/Denis/ML/IRIS_predspectra_intermediate_tf2/%s/%s/log"%(self.dataset,self.name)# Directory name to save the log [log]")
-        self.results_dir = "C:/Users/Denis/ML/IRIS_predspectra_intermediate_tf2/%s/%s/results"%(self.dataset,self.name)# Directory name to save the image samples [samples]")
-        self.train1 = True# True for training phase 1 (with BN) [False]")
-        self.train2 = False# True for training phase 2 (without BN) : Fine-tuning [False]")
-        self.preload_train = False# True for loading a pre-trained model before training, False for testing [False]")
-        self.testload_FINE = False# True for loading a trained model with FINE procedure, False for loading a non FINE model [True]")
-        self.test = True# True for testing directly at the end of training")
-        self.test_ds = '_'.join(['TE','TEL'])# chosen datasets for tests ['TR', 'VA', 'TE', 'TEL']")
-        self.with_features = False# whether features should be investigated
-        self.add_classifier = False# True to add classification stats (it will use the params from main_classify.py).")
-        self.classes = '_'.join(['PB'])# May be overriden by 'classes_and_inclusions', labels of classification ['QS','AR','PF','FL'] OR ['QS','AR-PF-FL']..")
-        self.class_inclusions = '_'.join([''])# inclusions for classification '_'.join(['QS<AR']) OR [QS<AR, QS<PF, QS<FL] OR ['']")
-        self.noclass = None# None or name for eventual events not sampling from 'classes' labels (will be assumed to output 0 values for the classifier)")
-        self.add_centercount = False# True to add centers stats (it will use the params from main_classify.py).")
-        self.predict = False# True for predicting number_predict from each chosen dataset predict_ds")
-        self.predict_ds = '_'.join(['TR', 'VAL', 'TE', 'TEL'])# chosen datasets for predictions ['TR', 'VAL', 'TE', 'TEL']")
-        self.number_predict = 4# The maximum number of predictions to do")
-        self.show_res = True# True for showing results at the end")
-        self.cosmic_to_mean = False# True for putting cosmic rays to the mean value") # V2
-        self.cosmic_t = 2000# Threshold in DN/s for cosmic rays [2000]")
-        self.show_dist_polar = False# Whether to show distribution in a polar way or not")
-        self.fig_form = 'pdf'# Format for saved figures in ['png', 'ps', 'pdf', 'svg']")
-        self.backg_color = False# Whether to colorize backgrounds or not")
-        self.frame_res = False# To frame marginal results in figures
-        # self.feat_legends = '__'.join(feat_legends)# list of the features legends")
+FLAGS = flags.FLAGS
 
-FLAGS = Flags()
-# FLAGS.feat_legends = FLAGS.feat_legends.split('__')
+flags.DEFINE_string("model_type", "PCUNet", "name of the model to user ['PCUNet'], ['LSTM'], ['GRU'], ..")
+flags.DEFINE_boolean("with_centerloss", False, "whether to add a term in the total loss optimizing the proximity to the centers")
+flags.DEFINE_boolean("debug", True, "True to use debug mode (1 epoch and 1st item of generator for test)")
+flags.DEFINE_integer("epoch", 100, "Epoch to train [25]")
+flags.DEFINE_integer("batch_size", 4, "The size of batch images [4]")
+flags.DEFINE_boolean("batch_norm", True, "True for the model with batch_normalzation")
+flags.DEFINE_float("learning_rate_BN", 0.0002, "Learning rate of for adam with BN (phase 1) [0.0002]")
+flags.DEFINE_float("learning_rate_FINE", 0.00005, "Learning rate of for adam without BN (phase 2 - Fine tuning) [0.00005]")
+flags.DEFINE_string("dataset", "pb_2C", "The name of dataset [iris_level_2C, al_2C, pb_2C]")
+flags.DEFINE_string("dataset_address", 'C:/Users/Denis/ML/MTS_Data/PEMS-BAY/data_2c', "The path of dataset")
+flags.DEFINE_boolean("given_tvt", True, "Whether the data is already separated in 'train' 'valid' 'test' (these should appear in the file names)")
+flags.DEFINE_float("train_ratio", 0.7, "ratio of dataset to use for training [0.7]")
+flags.DEFINE_float("test_ratio", 0.25, "ratio of dataset to use for testing [0.25]")
+flags.DEFINE_integer("label_length", 325, "The length of spectra. [240 (for Mghk), 137 (for al), 370 (for ld)))]")
+if FLAGS.model_type == "NBeats":
+    flags.DEFINE_integer("n_blocks", 2,"number of blocks for NBeats")
+flags.DEFINE_float("mask_ratio", 0.25, "ending ratio of the timesequences to be masked in time / max ratio is random_ratio in True")
+flags.DEFINE_boolean("random_ratio", False, "True for random ending ratio of the timesequences to be masked in time (with max value = mask_ratio)")
+flags.DEFINE_string("labels", '_'.join(['PB']), "label for training ['QS','AR','PF','FL'] (these should appear in the filename[:2])")
+flags.DEFINE_string("nolabel", None, "allow to sample from unlabeled data and label it eg. 'nolabel'")
+flags.DEFINE_string("test_labels", '_'.join(['PB']), "label for testing ['QS','AR','PF','FL']")
+flags.DEFINE_string("name", 'model%s%s_B%i_M%i_R%i_%s'%([FLAGS.model_type+"%s"%['','star'][int(FLAGS.with_centerloss)],"Mghk%s"%['','star'][int(FLAGS.with_centerloss)]][int(FLAGS.model_type=="PCUNet")], FLAGS.dataset[-2:], FLAGS.batch_size, int(100*FLAGS.mask_ratio), int(FLAGS.random_ratio), FLAGS.labels), "The name of the model")
+flags.DEFINE_string("checkpoint_dir", "C:/Users/Denis/ML/IRIS_predspectra_intermediate_tf2/%s/%s/checkpoint"%(FLAGS.dataset,self.name), "Directory name to save the checkpoints [checkpoint]")
+flags.DEFINE_string("logs_dir", "C:/Users/Denis/ML/IRIS_predspectra_intermediate_tf2/%s/%s/log"%(FLAGS.dataset,FLAGS.name), "Directory name to save the log [log]")
+flags.DEFINE_string("results_dir", "C:/Users/Denis/ML/IRIS_predspectra_intermediate_tf2/%s/%s/results"%(FLAGS.dataset,FLAGS.name), "Directory name to save the image samples [samples]")
+flags.DEFINE_boolean("train1", True, "True for training phase 1 (with BN) [False]")
+flags.DEFINE_boolean("train2", False, "True for training phase 2 (without BN) : Fine-tuning [False]")
+flags.DEFINE_boolean("preload_train", False, "True for loading a pre-trained model before training, False for testing [False]")
+flags.DEFINE_boolean("testload_FINE", False, "True for loading a trained model with FINE procedure, False for loading a non FINE model [True]")
+flags.DEFINE_boolean("test", True, "True for testing directly at the end of training")
+flags.DEFINE_string("test_ds", '_'.join(['TE','TEL']), "chosen datasets for tests ['TR', 'VA', 'TE', 'TEL']")
+flags.DEFINE_boolean("with_features", False, "whether features should be investigated")
+flags.DEFINE_boolean("add_classifier", False, "True to add classification stats (it will use the params from main_classify.py).")
+flags.DEFINE_string("classes", '_'.join(['PB']), "May be overriden by 'classes_and_inclusions', labels of classification ['QS','AR','PF','FL'] OR ['QS','AR-PF-FL']..")
+flags.DEFINE_string("class_inclusions", '_'.join(['']), "inclusions for classification '_'.join(['QS<AR']) OR [QS<AR, QS<PF, QS<FL] OR ['']")
+flags.DEFINE_string("noclass", None, "None or name for eventual events not sampling from 'classes' labels (will be assumed to output 0 values for the classifier)")
+flags.DEFINE_boolean("add_centercount", False, "True to add centers stats (it will use the params from main_classify.py).")
+flags.DEFINE_boolean("predict", False, "True for predicting number_predict from each chosen dataset predict_ds")
+flags.DEFINE_string("predict_ds", '_'.join(['TR', 'VAL', 'TE', 'TEL']), "chosen datasets for predictions ['TR', 'VAL', 'TE', 'TEL']")
+flags.DEFINE_integer("number_predict", 4, "The maximum number of predictions to do")
+flags.DEFINE_boolean("show_res", True, "True for showing results at the end")
+flags.DEFINE_boolean("cosmic_to_mean", False, "True for putting cosmic rays to the mean value") # V2
+flags.DEFINE_integer("cosmic_t", 2000, "Threshold in DN/s for cosmic rays [2000]")
+flags.DEFINE_boolean("show_dist_polar", False, "Whether to show distribution in a polar way or not")
+flags.DEFINE_string("fig_form", 'pdf', "Format for saved figures in ['png', 'ps', 'pdf', 'svg']")
+flags.DEFINE_boolean("backg_color", False, "Whether to colorize backgrounds or not")
+flags.DEFINE_boolean("frame_res", False, "To frame marginal results in figures")
 
 def main():
     """
@@ -176,4 +175,4 @@ def main():
         spectral_predictor.tests()
 
 if __name__ == '__main__':
-    main()
+  app.run(main)
