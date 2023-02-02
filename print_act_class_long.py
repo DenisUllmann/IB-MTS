517a57c484c9ca87717c73637f219572e1f8302c
DenisUllmann <31477905+DenisUllmann@users.noreply.github.com>
2023-02-02 17:21:21 +0100

    Update print_act_class_long.py

--- print_act_class_long.py
+++ print_act_class_long.py
@@ -5,12 +5,23 @@ Created on Tue Nov 22 11:32:52 2022
 @author: Denis
 """
 import numpy as np
-res = np.load('compare_act_long.npz', allow_pickle=True)
-dres = res['res'].all()
-res.close()
-mkey = 'ClassifiedInput VS ClassifiedOutput'
-for k in dres.keys():
-    print("{}".format(k))
-    for lab in dres[k][mkey].keys():
-        print(lab)
-        print(("{} : {:.1f}  /"+"{} : {:.3f}  /"*(len(dres[k][mkey][lab])-1)).format(*[vv  for v in dres[k][mkey][lab].items() for vv in v]))
+from absl import app
+from absl import flags
+from absl import logging
+
+FLAGS = flags.FLAGS
+flags.DEFINE_string("fname", 'compare_act_long.npz', "File name: path with name of the file to read")
+
+def main():
+    res = np.load(FLAGS.fname, allow_pickle=True)
+    dres = res['res'].all()
+    res.close()
+    mkey = 'ClassifiedInput VS ClassifiedOutput'
+    for k in dres.keys():
+        print("{}".format(k))
+        for lab in dres[k][mkey].keys():
+            print(lab)
+            print(("{} : {:.1f}  /"+"{} : {:.3f}  /"*(len(dres[k][mkey][lab])-1)).format(*[vv  for v in dres[k][mkey][lab].items() for vv in v]))
+
+if __name__ == '__main__':
+    app.run(main)
