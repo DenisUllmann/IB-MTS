9f84b3d760f80f124dab9c0b54f175e6c55885c0
DenisUllmann <31477905+DenisUllmann@users.noreply.github.com>
2023-02-02 16:58:17 +0100

    Update get_centers_accthss.py

--- get_centers_accthss.py
+++ get_centers_accthss.py
@@ -5,13 +5,24 @@ Created on Tue Nov 22 12:39:36 2022
 @author: Denis
 """
 import numpy as np
-res = np.load('compare_centers.npz', allow_pickle=True)
-dres = res['res'].all()
-res.close()
-for k in dres.keys():
-    print("{}".format(k))
-    for lab in dres[k].keys():
-        print("{}".format(lab))
-        for met in dres[k][lab].keys():
-            print(["{} : {:.1f}", "{} : {:.3f}"][int('ss' in met)].format(
-                met, dres[k][lab][met]))
+from absl import app
+from absl import flags
+from absl import logging
+
+FLAGS = flags.FLAGS
+flags.DEFINE_string("fname", 'compare_centers.npz', "File name: path with name of the file to read")
+
+def main():
+    res = np.load(FLAGS.fname, allow_pickle=True)
+    dres = res['res'].all()
+    res.close()
+    for k in dres.keys():
+        print("{}".format(k))
+        for lab in dres[k].keys():
+            print("{}".format(lab))
+            for met in dres[k][lab].keys():
+                print(["{} : {:.1f}", "{} : {:.3f}"][int('ss' in met)].format(
+                    met, dres[k][lab][met]))
+
+if __name__ == '__main__':
+    app.run(main)
