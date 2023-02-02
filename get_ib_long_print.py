e1886b9a68f11004555e3f720e8a35707d4230be
DenisUllmann <31477905+DenisUllmann@users.noreply.github.com>
2023-02-02 17:05:25 +0100

    Update get_ib_long_print.py

--- get_ib_long_print.py
+++ get_ib_long_print.py
@@ -5,12 +5,23 @@ Created on Fri Nov 18 14:23:26 2022
 @author: Denis
 """
 import numpy as np
-res = np.load('compare_ib_long.npz', allow_pickle=True)
-dres = res['res'].all()
-res.close()
-for k in dres.keys():
-    print("{}".format(k))
-    for dt in dres[k].keys():
-        print("{}".format(dt))
-        for met in dres[k][dt].keys():
-            print("{} : {:.3f} /".format(met,dres[k][dt][met]))
+from absl import app
+from absl import flags
+from absl import logging
+
+FLAGS = flags.FLAGS
+flags.DEFINE_string("fname", 'compare_ib_long.npz', "File name: path with name of the file to read")
+
+def main():
+    res = np.load(FLAGS.fname, allow_pickle=True)
+    dres = res['res'].all()
+    res.close()
+    for k in dres.keys():
+        print("{}".format(k))
+        for dt in dres[k].keys():
+            print("{}".format(dt))
+            for met in dres[k][dt].keys():
+                print("{} : {:.3f} /".format(met,dres[k][dt][met]))
+
+if __name__ == '__main__':
+    app.run(main)
