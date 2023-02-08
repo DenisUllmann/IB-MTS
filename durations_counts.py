a4ffc2bd164d4da01133a1ea4d9faea08476339a
DenisUllmann <31477905+DenisUllmann@users.noreply.github.com>
2023-02-08 16:05:17 +0100

    Add files via upload

---
+++ durations_counts.py
@@ -0,0 +1,272 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Dec 13 14:29:54 2022
+
+@author: Denis
+"""
+# TO RUN under tf2_10 env and Denis\ML\IRIS_predspectra_intermediate_tf2 dir
+import os
+import tensorflow as tf
+import numpy as np
+
+from models import SP_PCUNet
+
+# name of the features studied and type of the label ticks for the graphs
+feat_legends = [('intensity','%.1f'),
+                ('triplet intensity','%.2f'),
+                ('line center','%.1f'),
+                ('line width','int'),
+                ('line asymmetry','%.2f'),
+                ('total_continium','int'),
+                ('triplet emission','int'),
+                ('k/h ratio integrated','%.2f'),
+                ('kh ratio max','%.2f'),
+                ('k hight','%.2f'),
+                ('peak ratios','int'),
+                ('peak separation','int')]
+
+# SETTINGS
+
+# To output and npz file with the physical features onthe data
+output_npz_features = False
+
+# To eventualy define only one classifier with settings 
+# 'classes' and 'class_inclusions'
+#classes_and_inclusions = None
+
+# To define several classifiers list[(classes, inclusions, noclass), ..]
+# For model trained with all labels
+classes_and_inclusions_addnoclass = list(zip(
+    ['_'.join(clss) for clss in [
+        ['QS','AR','PF','FL'],
+        ['QS','AR','PF','FL'],
+        ['QS','AR','PF','FL'],
+        ['QS','AR','PF','FL'],
+        ['QS'],
+        ['AR'],
+        ['PF'],
+        ['FL']]],
+    ['_'.join(incs) for incs in [
+        ['QS<AR'],
+        ['QS<AR'],
+        [''],
+        [''],
+        [''],
+        [''],
+        [''],
+        ['']]],
+    [
+      None,
+      'noclass',
+      None,
+      'noclass',
+      'noclass',
+      'noclass',
+      'noclass',
+      'noclass']))
+# For model trained just with labels not in the inclusions above
+# classes_and_inclusions_addnoclass = list(zip(
+#     ['_'.join(clss) for clss in [
+#         ['QS','AR','PF','FL'],
+#         ['QS','AR','PF','FL'],
+#         ['QS'],
+#         ['AR'],
+#         ['PF'],
+#         ['FL']]],
+#     ['_'.join(incs) for incs in [
+#         [''],
+#         [''],
+#         [''],
+#         [''],
+#         [''],
+#         ['']]],
+#     [None,
+#      'noclass',
+#      'noclass',
+#      'noclass',
+#      'noclass',
+#      'noclass']))
+
+class Flags(object):
+    def __init__(self):
+        self.model_type = "PCUNet"# name of the model to user ['PCUNet'], ['LSTM'], ['GRU'], ..")
+        self.with_centerloss = False# whether to add a term in the total loss optimizing the proximity to the centers
+        self.debug = True# True to use debug mode (1 epoch and 1st item of generator for test)")
+        self.epoch = 100# Epoch to train [25]")
+        self.batch_size = 4# The size of batch images [4]")
+        self.batch_norm = True# True for the model with batch_normalzation")
+        self.learning_rate_BN = 0.0002# Learning rate of for adam with BN (phase 1) [0.0002]")
+        self.learning_rate_FINE = 0.00005# Learning rate of for adam without BN (phase 2 - Fine tuning) [0.00005]")
+        self.dataset = "iris_level_2C"# The name of dataset [iris_level_2C, UCSD, mnist]")
+        self.dataset_address = 'C:/Users/Denis/ML/MTS_Data/PEMS-BAY/data_2c'# The path of dataset")
+        self.given_tvt = True# Whether the data is already separated in 'train' 'valid' 'test' (these should appear in the file names)
+        self.train_ratio = 0.7# ratio of dataset to use for training [0.7]")
+        self.test_ratio = 0.25# ratio of dataset to use for testing [0.25]")
+        self.label_length = 240# The length of spectra. [240 (for Mghk), 137 (for al), 370 (for ld)))]")
+        if self.model_type == "NBeats":
+            self.n_blocks = int(65714097/50400)
+        self.mask_ratio = 0.25# ending ratio of the timesequences to be masked in time / max ratio is random_ratio in True")
+        self.random_ratio = False# True for random ending ratio of the timesequences to be masked in time (with max value = mask_ratio)")
+        self.labels = '_'.join(['QS','AR','PF','FL'])# label for training ['QS','AR','PF','FL'] (these should appear in the filename[:2])")
+        self.nolabel = None# allow to sample from unlabeled data and label it eg. 'nolabel'")
+        self.test_labels = '_'.join(['QS','AR','PF','FL'])# label for testing ['QS','AR','PF','FL']")
+        self.name = 'model%s%s_B%i_M%i_R%i_%s'%([self.model_type+"%s"%['','star'][int(self.with_centerloss)],"Mghk%s"%['','star'][int(self.with_centerloss)]][int(self.model_type=="PCUNet")], self.dataset[-2:], self.batch_size, int(100*self.mask_ratio), int(self.random_ratio), self.labels)# The name of the model")
+        self.checkpoint_dir = "C:/Users/Denis/ML/IRIS_predspectra_intermediate_tf2/%s/%s/checkpoint"%(self.dataset,self.name)# Directory name to save the checkpoints [checkpoint]")
+        self.logs_dir = "C:/Users/Denis/ML/IRIS_predspectra_intermediate_tf2/%s/%s/log"%(self.dataset,self.name)# Directory name to save the log [log]")
+        self.results_dir = "C:/Users/Denis/ML/IRIS_predspectra_intermediate_tf2/%s/%s/results"%(self.dataset,self.name)# Directory name to save the image samples [samples]")
+        self.train1 = True# True for training phase 1 (with BN) [False]")
+        self.train2 = False# True for training phase 2 (without BN) : Fine-tuning [False]")
+        self.preload_train = False# True for loading a pre-trained model before training, False for testing [False]")
+        self.testload_FINE = False# True for loading a trained model with FINE procedure, False for loading a non FINE model [True]")
+        self.test = True# True for testing directly at the end of training")
+        self.test_ds = '_'.join(['TE','TEL'])# chosen datasets for tests ['TR', 'VA', 'TE', 'TEL']")
+        self.with_features = False# whether features should be investigated
+        self.add_classifier = False# True to add classification stats (it will use the params from main_classify.py).")
+        self.classes = '_'.join(['PB'])# May be overriden by 'classes_and_inclusions', labels of classification ['QS','AR','PF','FL'] OR ['QS','AR-PF-FL']..")
+        self.class_inclusions = '_'.join([''])# inclusions for classification '_'.join(['QS<AR']) OR [QS<AR, QS<PF, QS<FL] OR ['']")
+        self.noclass = None# None or name for eventual events not sampling from 'classes' labels (will be assumed to output 0 values for the classifier)")
+        self.add_centercount = False# True to add centers stats (it will use the params from main_classify.py).")
+        self.predict = False# True for predicting number_predict from each chosen dataset predict_ds")
+        self.predict_ds = '_'.join(['TR', 'VAL', 'TE', 'TEL'])# chosen datasets for predictions ['TR', 'VAL', 'TE', 'TEL']")
+        self.number_predict = 4# The maximum number of predictions to do")
+        self.show_res = True# True for showing results at the end")
+        self.cosmic_to_mean = False# True for putting cosmic rays to the mean value") # V2
+        self.cosmic_t = 2000# Threshold in DN/s for cosmic rays [2000]")
+        self.show_dist_polar = False# Whether to show distribution in a polar way or not")
+        self.fig_form = 'pdf'# Format for saved figures in ['png', 'ps', 'pdf', 'svg']")
+        self.backg_color = False# Whether to colorize backgrounds or not")
+        self.frame_res = False# To frame marginal results in figures
+        # self.feat_legends = '__'.join(feat_legends)# list of the features legends")
+
+FLAGS = Flags()
+import os
+import gc
+import datetime
+import numpy as np
+import pandas as pd
+import random
+import itertools
+import zipfile
+import io
+import time
+
+from copy import deepcopy
+from tqdm import tqdm
+from glob import glob
+from natsort import natsorted
+import seaborn as sns
+
+import tensorflow as tf
+from keras.preprocessing.image import ImageDataGenerator
+from keras.callbacks import TensorBoard, ModelCheckpoint, LambdaCallback
+#from keras_tqdm import TQDMCallback
+
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+plt.ioff()
+import matplotlib.gridspec as gridspec
+#from mpl_toolkits.axes_grid1 import make_axes_locatable
+# from matplotlib.ticker import NullFormatter
+from skimage.metrics import structural_similarity as ssim
+
+from libs.pconv_model import PConvUnet
+from libs.lstm_model import LSTM
+from libs.lstmsimple_model import LSTM as LSTMS
+from libs.gru_model import GRU
+from libs.grusimple_model import GRU as GRUS
+from libs.nbeats_model import NBeats
+
+from dataset.data_process import kinter, forplot_assignement_accuracy, kcentroids_equal, to_kcentroid_seq, chunkdata_for_longpredict, retrieve_traintimeseq, create_labelines_timeseq_dataset, convertdata_for_training, no_cosmic, rescale_data_by_seqs
+from featuring.brandon_features import feature_transform, Mg_settings
+from sklearn.metrics import confusion_matrix#, ConfusionMatrixDisplay
+
+class_parms = None
+from featuring.mts_metrics import NPMtsMetrics
+from featuring.class_n2_metrics import tss_hss_all
+try:
+    from main_classify import Settings, update_settings_fromclass
+    from models_classify import SP_Conv_Dense, create_class_mask
+    from featuring.center_stat import NPJointCenterStat#, NPCenterStat, CenterStat
+    from libs.class_pconv_model import NP_CategoricalCrossentropy, NP_BinaryCrossentropy, NP_CategoricalAccuracy, NP_BinaryAccuracy
+    from libs.countdict3k_acc import NPAccuracyOverTime3D
+except:
+    print("Could not import libraries on centers and classification")
+else:
+    print("successfuly imported libraries on centers and classification")
+
+now = datetime.datetime.now
+plt.rcParams.update({'font.size': 10})
+plt.rcParams.update({'font.family': 'Cambria'})
+manual_mode = True
+change_traindata = True # whether to save/overwrite data_longformat.npz
+config = FLAGS
+
+class AugmentingDataGenerator(ImageDataGenerator):
+    def flow_from_data(self, seq, position, mask_ratio, random_ratio, *args, **kwargs):
+        generator = super().flow((seq, position), *args, **kwargs)
+        while True:
+
+            # Get augmentend image samples
+            ori, position = next(generator)
+
+            # Get masks for each image sample            
+            mask = np.ones(ori.shape)
+            if random_ratio:
+                for i in range(mask.shape[0]):
+                    mask_ratio_s = random.uniform(0.03,mask_ratio)
+                    mask[i, -int(mask.shape[0] * mask_ratio_s):, :, :] = 0
+            else:
+                mask[:,-int(mask.shape[1] * mask_ratio):, :, :] = 0
+
+            # Apply masks to all image sample
+            masked = deepcopy(ori)
+#            masked[mask==0] = np.mean(masked[mask==1]) #value of the maked data is not 1 because the data could be much smaller than the max value of all data (1)
+            masked[mask==0] = 1
+
+            # Yield ([ori, masl],  ori) training batches
+            gc.collect()
+            yield [masked, mask, position], ori
+            
+self = SP_PCUNet(FLAGS, 
+    classes_and_inclusions_addnoclass=classes_and_inclusions_addnoclass, 
+    feat_legends=feat_legends)
+self.dataset = 'iris_level_2C'
+self.checkpoint_dir = 'C:/Users/Denis/HPC/Yggdrasil/iris_train_data'
+self.label_length = 240
+self.labels = ['QS', 'AR', 'PF', 'FL']
+print("Load data from {}".format(os.path.join(self.checkpoint_dir,'data_longformat.npz')))
+data_info = np.load(os.path.join(self.checkpoint_dir,'data_longformat.npz'), allow_pickle = True)
+keys = [k.replace('data_', '') for k in list(data_info.keys()) if 'data_' in k]
+self.data_pack = {k: (data_info['data_'+k], data_info['position_'+k]) for k in keys}
+data_info.close()
+change_traindata = False
+
+counts = {}
+for k in ['TE_QS','TE_AR','TE_FL']:
+    counts[k] = []
+    
+self.data_pack['TE_QS']
+len(self.data_pack['TE_QS'])
+self.data_pack['TE_QS'][0][0]
+self.data_pack['TE_QS'][0][0].shape
+self.data_pack['TE_QS'][1][0].shape
+self.data_pack['TE_QS'][1][0]
+self.data_pack['TE_QS'][1][0][0]
+self.data_pack['TE_QS'][1][0][1]
+        
+for k in ['TE_QS', 'TE_AR', 'TE_FL']:
+    for elem_pos in zip(*self.data_pack[k]):
+        counts['TE_%s'%elem_pos[1][1][:2]].append(elem_pos[0].shape[0]-int(240*.75))
+        
+plt.figure(figsize=(5,1.6))
+for k in counts.keys():
+    plt.hist(counts[k], bins='stone', label=k.replace('TE_', ''))
+plt.yscale('log')
+plt.legend(loc='upper center')
+plt.xlabel('predicted time-steps')
+plt.ylabel('counts')
+plt.gca().set_aspect(50)
+plt.tight_layout()
+
+plt.savefig('durations_counts.pdf')
