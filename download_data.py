201500a0b19cb356fcc0fa619ea094d91526d106
DenisUllmann <31477905+DenisUllmann@users.noreply.github.com>
2023-02-08 17:12:47 +0100

    Update download_data.py

--- download_data.py
+++ download_data.py
@@ -8,18 +8,24 @@ FLAGS = flags.FLAGS
 
 flags.DEFINE_string("data", 'iris', "Dataset name ['iris', 'al', 'pb']")
 flags.DEFINE_string("dir", os.path.join(os.path.dirname(os.path.abspath(__file__)), 'iris_data'), "Directory path where to load data")
+flags.DEFINE_boolean("prpr", True, "Whether to download preprocessed data")
 
 dois = {
-  'iris': '',
-  'al': '',
-  'pb': ''
+  'iris': ('',''), #key name: (preprocessed doi link, raw doi link)
+  'al': ('',''),
+  'pb': ('','')
   }
 
 def main():
   # download data
-  result = subprocess.run(['zenodo_get'], ['-d'], [dois[FLAGS.data]], ['-o'], [FLAGS.dir])
-  with py7zr.SevenZipFile("data_longformat.7z", 'r') as archive:
-    archive.extractall(path=FLAGS.dir)
+  if FLAGS.prpr:
+    result = subprocess.run(['zenodo_get'], ['-d'], [dois[FLAGS.data][0]], ['-o'], [FLAGS.dir])
+    with py7zr.SevenZipFile("data_longformat.7z", 'r') as archive:
+      archive.extractall(path=FLAGS.dir)
+  else:
+    result = subprocess.run(['zenodo_get'], ['-d'], [dois[FLAGS.data][1]], ['-o'], [FLAGS.dir])
+    with py7zr.SevenZipFile("data_raw.7z", 'r') as archive:
+      archive.extractall(path=FLAGS.dir)
 
 if __name__ == '__main__':
   app.run(main)
