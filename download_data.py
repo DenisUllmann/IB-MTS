80d67a0a7cc3cda803679e4139564c36c7c405cf
DenisUllmann <31477905+DenisUllmann@users.noreply.github.com>
2023-02-08 16:58:44 +0100

    Update download_data.py

--- download_data.py
+++ download_data.py
@@ -7,7 +7,7 @@ from absl import flags
 FLAGS = flags.FLAGS
 
 flags.DEFINE_string("data", 'iris', "Dataset name ['iris', 'al', 'pb']")
-flags.DEFINE_string("dir", os.path.join(os.path.dirname(os.path.abspath(__file__)), 'PrPr_data'), "Directory path where to load data")
+flags.DEFINE_string("dir", os.path.join(os.path.dirname(os.path.abspath(__file__)), 'iris_data'), "Directory path where to load data")
 
 dois = {
   'iris': '',
@@ -16,7 +16,10 @@ dois = {
   }
 
 def main():
-  result = subprocess.run(['zenodo_get'])
+  # download data
+  result = subprocess.run(['zenodo_get'], ['-d'], [dois[FLAGS.data]], ['-o'], [FLAGS.dir])
+  with py7zr.SevenZipFile("data_longformat.7z", 'r') as archive:
+    archive.extractall(path=FLAGS.dir)
 
 if __name__ == '__main__':
   app.run(main)
