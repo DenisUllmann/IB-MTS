e50f3db12661095c7a1422fd8e62a14dbf97a1b1
DenisUllmann <31477905+DenisUllmann@users.noreply.github.com>
2023-02-24 14:29:30 +0100

    Update download_data.py
    update preprocessed links

--- download_data.py
+++ download_data.py
@@ -11,18 +11,15 @@ flags.DEFINE_string("dir", os.path.join(os.path.dirname(os.path.abspath(__file__
 flags.DEFINE_boolean("prpr", True, "Whether to download preprocessed data")
 
 dois = {
-  'iris': ('',''), #key name: (preprocessed doi link, raw doi link)
-  'al': ('',''),
-  'pb': ('','')
+  'iris': ('10.5281/zenodo.7524572',''), #key name: (preprocessed doi link, raw doi link)
+  'al': ('10.5281/zenodo.7674274',''),
+  'pb': ('10.5281/zenodo.7674366','')
   }
 
 def main():
   # download data
   if FLAGS.prpr:
     result = subprocess.run(['zenodo_get'], ['-d'], [dois[FLAGS.data][0]], ['-o'], [FLAGS.dir])
-    with py7zr.SevenZipFile("data_longformat.7z", 'r') as archive:
-      archive.extractall(path=FLAGS.dir)
-    os.remove(os.path.join(write_dir, "data_longformat.7z"))
   else:
     result = subprocess.run(['zenodo_get'], ['-d'], [dois[FLAGS.data][1]], ['-o'], [FLAGS.dir])
     with py7zr.SevenZipFile("data_raw.7z", 'r') as archive:
